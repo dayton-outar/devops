f5391fa8e1e1641144d6439f54cadebd09f0be6f
Dayton Outar <dayton.outar@gmail.com>
2021-01-17 18:55:26 -0500

    SQL Server does not respond well to password with $$

--- mssql-sql-server/restoring-database.sh
+++ mssql-sql-server/restoring-database.sh
@@ -1,3 +1,3 @@
 # Advised by https://docs.microsoft.com/en-us/sql/linux/sql-server-linux-backup-and-restore-database?view=sql-server-ver15
 # Adapted from https://docs.microsoft.com/en-us/sql/linux/tutorial-restore-backup-in-sql-server-container?view=sql-server-ver15
-sqlcmd -S localhost,1401 -U SA -P "br0wnPu$$y" -Q "RESTORE DATABASE [blitz] FROM DISK = N'/var/opt/mssql/data/BLITZ.bak' WITH MOVE 'BLITZ' TO '/var/opt/mssql/data/BLITZ.mdf', MOVE 'BLITZ_log' TO '/var/opt/mssql/data/BLITZ_log.ldf'"
+sqlcmd -S localhost,1401 -U SA -P "Password44" -Q "RESTORE DATABASE [blitz] FROM DISK = N'/var/opt/mssql/data/BLITZ.bak' WITH MOVE 'BLITZ' TO '/var/opt/mssql/data/BLITZ.mdf', MOVE 'BLITZ_log' TO '/var/opt/mssql/data/BLITZ_log.ldf'"
