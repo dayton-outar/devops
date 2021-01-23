c59f9131b02d1905a690c93706bdf01545c51c5e
Dayton Outar <dayton.outar@gmail.com>
2021-01-23 06:27:36 -0500

    Reference and fixes to docker-composer

--- mssql-sql-server/restoring-database.sh
+++ mssql-sql-server/restoring-database.sh
@@ -1,3 +1,4 @@
+# Docker Images for Microsoft SQL Server found here: https://hub.docker.com/_/microsoft-mssql-server
 # Advised by https://docs.microsoft.com/en-us/sql/linux/sql-server-linux-backup-and-restore-database?view=sql-server-ver15
 # Adapted from https://docs.microsoft.com/en-us/sql/linux/tutorial-restore-backup-in-sql-server-container?view=sql-server-ver15
 sqlcmd -S localhost,1401 -U SA -P "Password44" -Q "RESTORE DATABASE [blitz] FROM DISK = N'/var/opt/mssql/data/BLITZ.bak' WITH MOVE 'BLITZ' TO '/var/opt/mssql/data/BLITZ.mdf', MOVE 'BLITZ_log' TO '/var/opt/mssql/data/BLITZ_log.ldf'"
